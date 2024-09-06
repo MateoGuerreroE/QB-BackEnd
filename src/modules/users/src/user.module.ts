9581878e12c1d225e768054139f9b2f3999e936c
Mateo Guerrero <140415694+MateoGuerreroE@users.noreply.github.com>
2024-09-05 22:17:14 -0500

    Repository - Initial Connection To Database

--- src/modules/users/src/user.module.ts
+++ src/modules/users/src/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UtilsModule } from 'src/modules/utils';
 import { UserController } from './user.controller';
+import { RepositoryModule } from 'src/modules/repository';
 
 @Module({
-  imports: [UtilsModule],
+  imports: [UtilsModule, RepositoryModule],
   controllers: [UserController],
 })
 export class UserModule {}
